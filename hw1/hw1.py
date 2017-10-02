32c365b618d3e8b1731a3790f9893e7dea4fc435
Stephanie Xie <sxie@umass.edu>
2017-10-02 16:55:32 -0400

    change visualization technique

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,41 +4,59 @@ Stephanie Xie
 CSC 350
 
 Collaborators: Emma Stephenson, Zainab Aqdas, Eleanor Ewing
-
+References: StackOverflow
 """
-import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
 def get_data_from_csv(csv):
-    raw_data = pd.read_csv(csv)
-    return raw_data
+    df = pd.read_csv(csv)
+    return df
 
 
-def fill_with_mean(raw_data):
-    data = raw_data.copy()
-    return data.fillna(data.mean())
+def fill_with_mean(df):
+    df_copy = df.copy()
+    return df_copy.fillna(df_copy.mean())
 
 
-def fill_with_zero(raw_data):
-    data = raw_data.copy()
-    return data.fillna(0)
+def fill_with_zero(df):
+    df_copy = df.copy()
+    return df_copy.fillna(0)
+
+
+def diff_df(df1, df1_name, df2, df2_name):
+    df1_copy = df1.copy()
+    df2_copy = df2.copy()
+
+    if df1_copy.equals(df2_copy):
+        return None
+    else:
+        ne_stacked = (df1_copy != df2_copy).stack()
+        changed = ne_stacked[ne_stacked]
+        changed.index.names = ['id', 'field']
+        diff_locations = np.where(df1_copy != df2_copy)
+        d1_diff = df1_copy.values[diff_locations]
+        d2_diff = df2_copy.values[diff_locations]
+        return pd.DataFrame({"{}".format(df1_name): d1_diff, "{}".format(df2_name): d2_diff},
+                            index=changed.index)
 
 
 def main():
-    complete_data = get_data_from_csv("Wholesale customers data.csv")
-    missing_data = get_data_from_csv("Wholesale customers data-missing.csv")
-    mean = fill_with_mean(missing_data)
-    zero = fill_with_zero(missing_data)
+    complete_df = get_data_from_csv("Wholesale customers data.csv")
+    missing_df = get_data_from_csv("Wholesale customers data-missing.csv")
+    means_df = fill_with_mean(missing_df)
+    zeros_df = fill_with_zero(missing_df)
 
-    # Comparing only two fields for easier visualization
-    plt.figure()
-    plt.xlabel('Fresh')
-    plt.ylabel('Frozen')
-    plt.scatter(complete_data['Fresh'], complete_data['Frozen'], color='Red')
-    plt.scatter(mean['Fresh'], mean['Frozen'], color='Green')
-    plt.scatter(zero['Fresh'], zero['Frozen'], color='Yellow')
-    plt.show()
+    complete_missing_diff = diff_df(complete_df, "complete", missing_df, "missing")
+    complete_means_diff = diff_df(complete_df, "complete", means_df, "means")
+    complete_zeros_diff = diff_df(complete_df, "complete", zeros_df, "zeros")
+
+    print(complete_missing_diff)
+    print("\n-------------\n")
+    print(complete_means_diff)
+    print("\n-------------\n")
+    print(complete_zeros_diff)
 
 
 if __name__ == '__main__':
